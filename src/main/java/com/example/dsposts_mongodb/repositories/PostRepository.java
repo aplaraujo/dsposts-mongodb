bdba3986f34664e86fade9d1c2c63729259b1bd0
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-23 20:40:49 -0300

    Get post by title, query operators

--- src/main/java/com/example/dsposts_mongodb/repositories/PostRepository.java
+++ src/main/java/com/example/dsposts_mongodb/repositories/PostRepository.java
@@ -2,9 +2,13 @@ package com.example.dsposts_mongodb.repositories;
 
 import com.example.dsposts_mongodb.models.entities.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
