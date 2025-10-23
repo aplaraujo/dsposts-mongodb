4ac809bd4623abd03b8e0472ae90cebf1144957c
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-23 08:56:25 -0300

    first commit

---
+++ src/main/java/com/example/dsposts_mongodb/DspostsMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.dsposts_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspostsMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspostsMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dsposts_mongodb/DspostsMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsposts_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspostsMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
